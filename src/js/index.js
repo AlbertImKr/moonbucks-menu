7c5c2706f9714bb0615b4773713982fb23e7407e
ALbertIM0427 <guang7226961@naver.com>
2023-10-12 08:21:14 +0900

    feat: 메뉴 수정/사젝 기능

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,34 @@
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
+
+    const updateMenuCount = () => {
+        const menuCount = $("#espresso-menu-list").querySelectorAll("li").length
+        $(".menu-count").innerText = `총 ${menuCount} 개`
+    }
+
+    $("#espresso-menu-list").addEventListener("click", (e) => {
+
+        if (e.target.classList.contains("menu-edit-button")) {
+            const $menuName = e.target
+                .closest("li")
+                .querySelector(".menu-name")
+            const updatedMenuName = prompt(
+                "메뉴명을 수정하세요",
+                $menuName.innerText
+            )
+            $menuName.innerText = updatedMenuName
+        }
+
+        if (e.target.classList.contains("menu-remove-button")) {
+            if (confirm("정말 삭제하시겠습니까?")) {
+                const $menuName = e.target
+                    .closest("li").remove()
+            }
+            updateMenuCount()
+        }
+    })
+
     $("#espresso-menu-form").addEventListener("submit", (e) => {
         e.preventDefault();
     });
@@ -38,8 +66,7 @@ function App() {
             menuItemTemplate(espressoMenuName)
         );
 
-        const menuCount = $("#espresso-menu-list").querySelectorAll("li").length
-        $(".menu-count").innerText = `총 ${menuCount} 개`
+        updateMenuCount()
         $("#espresso-menu-name").value = ""
     }
 
